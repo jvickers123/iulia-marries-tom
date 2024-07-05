9c8d0f810c188102cded73ce2429f8df81b8f61f
jonathan-vickers-wttech <jonathan.vickers@wundermanthompson.com>
2024-07-05 13:48:21 +0100

    update timings

--- src/components/Timings.tsx
+++ src/components/Timings.tsx
@@ -13,8 +13,8 @@ const Info = ({
     <ModalContentContainer variant={ModalVariant.INFO}>
       <h2 className="info__heading">Timings</h2>
 
-      <p className="info__para info__para--green">11:30 - Day guests arrive</p>
-      <p className="info__para info__para--green">12:00 - Ceremony </p>
+      <p className="info__para info__para--green">12:15 - Day guests arrive</p>
+      <p className="info__para info__para--green">12:30 - Ceremony </p>
       <p className="info__para info__para--green">13:00 - Drinks reception</p>
       <p className="info__para info__para--green">14:30 - Wedding breakfast</p>
       <br />
